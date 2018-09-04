663371caf21a5c70ba54934a3d45d70a8a882233
smit666 <smitthakur666@gmail.com>
2018-09-04 10:47:24 +0530

    Add project files.

---
+++ pdf.js
@@ -0,0 +1,21 @@
+﻿module.exports = function (callback, data) {
+    var jsreport = require('jsreport-core')();
+
+    jsreport.init().then(function () {
+        return jsreport.render({
+            template: {
+                content: '<h4>{{:foo}}</h4>',
+                engine: 'jsrender',
+                recipe: 'phantom-pdf' 
+                //dataDirectory: path.join('./Image/', data)
+            },
+            data: {
+                foo: data
+            }
+        }).then(function (resp) {
+            callback(/* error */ null, resp.content.toJSON().data);
+        });
+    }).catch(function (e) {
+        callback(/* error */ e, null);
+    })
+};
